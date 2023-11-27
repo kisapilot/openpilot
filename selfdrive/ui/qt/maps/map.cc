704f065bd805f09d7a8c8cda71d872f193104b8e
multikyd <multikyd@gmail.com>
2023-11-27 19:52:41 +0900

    mapbox style

--- selfdrive/ui/qt/maps/map.cc
+++ selfdrive/ui/qt/maps/map.cc
@@ -271,11 +271,11 @@ void MapWindow::initializeGL() {
   if (MAPBOX_STYLE == "0") {
     m_map->setStyleUrl("mapbox://styles/commaai/clkqztk0f00ou01qyhsa5bzpj"); // comma
   } else if (MAPBOX_STYLE == "1") {
-    m_map->setStyleUrl("mapbox://styles/multikyd/ckwbf0oig3swu14lc482wqvfz"); // kisapilot
+    m_map->setStyleUrl("mapbox://styles/multikyd/clpgruv1g005q01opetzq2loo"); // kisapilot
   } else if (MAPBOX_STYLE == "2" && !Params().get("MapboxStyleCustom").empty()) {
     m_map->setStyleUrl(MAPBOX_CUSTOM); // yours
   } else if (MAPBOX_STYLE == "3") {
-    m_map->setStyleUrl("mapbox://styles/multikyd/clpf0fgbo003r01px93i00y5p"); // sat
+    m_map->setStyleUrl("mapbox://styles/mapbox/satellite-streets-v12"); // sat
   } else {
     m_map->setStyleUrl("mapbox://styles/commaai/clkqztk0f00ou01qyhsa5bzpj"); // comma
   }
