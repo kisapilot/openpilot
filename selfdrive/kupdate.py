951a7c4a611438c464c9f03e691502b5a6c88228
multikyd <multikyd@gmail.com>
2023-12-17 20:27:14 +0900

    cal and live param reset after changing model

--- selfdrive/kupdate.py
+++ selfdrive/kupdate.py
@@ -291,6 +291,8 @@ def sw_update_thread(end_event, nv_queue):
             rvalue=result.poll()
             if rvalue == 0:
               p_order = 6
+              params.remove("CalibrationParams")
+              params.remove("LiveTorqueParameters")
               result=subprocess.Popen(command6, shell=True)
             else:
               lcount += 1
@@ -361,6 +363,8 @@ def sw_update_thread(end_event, nv_queue):
             rvalue=result.poll()
             if rvalue == 0:
               p_order = 5
+              params.remove("CalibrationParams")
+              params.remove("LiveTorqueParameters")
               result=subprocess.Popen(command5, shell=True)
             else:
               lcount += 1
