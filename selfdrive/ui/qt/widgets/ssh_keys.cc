72137965b8f3aa15094640792c7498d4020326be
multikyd <multikyd@gmail.com>
2024-02-18 23:23:35 +0900

    kisa

--- selfdrive/ui/qt/widgets/ssh_keys.cc
+++ selfdrive/ui/qt/widgets/ssh_keys.cc
@@ -7,7 +7,7 @@
 SshControl::SshControl() : ButtonControl(tr("SSH Keys"), "", tr("Warning: This grants SSH access to all public keys in your GitHub settings. Never enter a GitHub username other than your own. A comma employee will NEVER ask you to add their GitHub username.")) {
   username_label.setAlignment(Qt::AlignRight | Qt::AlignVCenter);
   username_label.setStyleSheet("color: #aaaaaa");
-  hlayout->insertWidget(1, &username_label);
+  hlayout->addWidget(&username_label);
 
   QObject::connect(this, &ButtonControl::clicked, [=]() {
     if (text() == tr("ADD")) {
