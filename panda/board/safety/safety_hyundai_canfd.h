b9bc5fbef52a4c2d3e4dc7faa89d2c80e998074c
multikyd <72329880+multikyd@users.noreply.github.com>
2024-04-29 20:41:03 +0900

    test

--- panda/board/safety/safety_hyundai_canfd.h
+++ panda/board/safety/safety_hyundai_canfd.h
@@ -209,8 +209,17 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *to_push) {
     // cruise state
     if ((addr == 0x1a0) && !hyundai_longitudinal) {
       // 1=enabled, 2=driver override
+      const int button_addr2 = hyundai_canfd_alt_buttons ? 0x1aa : 0x1cf;
+      bool main_button_stat = false;
+      if (addr == button_addr2) {
+        if (addr == 0x1cf) {
+          main_button_stat = GET_BIT(to_push, 19U);
+        } else {
+          main_button_stat = GET_BIT(to_push, 34U);
+        }
+      }
       int cruise_status = ((GET_BYTE(to_push, 8) >> 4) & 0x7U);
-      bool cruise_engaged = (cruise_status == 1) || (cruise_status == 2);
+      bool cruise_engaged = (cruise_status == 1) || (cruise_status == 2) || main_button_stat;
       hyundai_common_cruise_state_check(cruise_engaged);
     }
   }
