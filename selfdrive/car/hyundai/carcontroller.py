2c132203044d63ec0d3024470d9fdf662273b488
multikyd <72329880+multikyd@users.noreply.github.com>
2024-05-16 22:36:21 +0900

    driver override

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -1400,11 +1400,12 @@ class CarController(CarControllerBase):
 
         standstill = CS.out.cruiseState.standstill and 6 > CS.lead_distance > 0 and not resume
         # cruise standstill resume
-        if standstill:
-          if self.cruise_gap_prev == 0 and CS.cruiseGapSet != 1.0 and self.kisa_cruisegap_auto_adj and not self.gap_by_spd_on and not self.cruise_gap_set_init:
+        if standstill and not self.gap_by_spd_on:
+          print('test1')
+          if self.cruise_gap_prev == 0 and CS.cruiseGapSet != 1.0 and self.kisa_cruisegap_auto_adj and not self.cruise_gap_set_init:
             self.cruise_gap_prev = CS.cruiseGapSet
             self.cruise_gap_set_init = True
-          elif CS.cruiseGapSet != 1.0 and self.kisa_cruisegap_auto_adj and not self.gap_by_spd_on and self.cruise_gap_set_init:
+          elif CS.cruiseGapSet != 1.0 and self.kisa_cruisegap_auto_adj and self.cruise_gap_set_init:
             for _ in range(self.btn_count):
               can_sends.append(hyundaicanfd.create_buttons(self.packer, self.CP, self.CAN, CS.buttons_counter, Buttons.GAP_DIST, CS.cruise_btn_info))
             self.last_button_frame = self.frame
@@ -1454,7 +1455,7 @@ class CarController(CarControllerBase):
           self.try_early_stop_org_gap = 0
           self.refresh_time = 1
           self.cruise_gap_adjusting = False
-        elif self.gap_by_spd_on and self.gap_by_spd_on_sw_trg and CS.cruiseGapSet != self.gap_by_spd_gap[0] and ((CS.clu_Vanz < self.gap_by_spd_spd[0]+self.gap_by_spd_on_buffer1) or self.gap_by_spd_gap1) and not self.try_early_stop_retrieve and not CS.lead_objspd < 3:
+        elif self.gap_by_spd_on and self.gap_by_spd_on_sw_trg and CS.cruiseGapSet != self.gap_by_spd_gap[0] and ((CS.clu_Vanz < self.gap_by_spd_spd[0]+self.gap_by_spd_on_buffer1) or self.gap_by_spd_gap1) and not self.try_early_stop_retrieve and not CS.lead_objspd < 0:
           self.gap_by_spd_gap1 = True
           self.gap_by_spd_gap2 = False
           self.gap_by_spd_gap3 = False
@@ -1473,7 +1474,7 @@ class CarController(CarControllerBase):
             self.refresh_time = interp(CS.cruiseGapSet, [1,2,4], [2,1,3])
           elif self.gap_by_spd_gap[0] == 4:
             self.refresh_time = interp(CS.cruiseGapSet, [1,2,3], [3,2,1])
-        elif self.gap_by_spd_on and self.gap_by_spd_on_sw_trg and CS.cruiseGapSet != self.gap_by_spd_gap[1] and ((self.gap_by_spd_spd[0] <= CS.clu_Vanz < self.gap_by_spd_spd[1]+self.gap_by_spd_on_buffer2) or self.gap_by_spd_gap2) and not self.try_early_stop_retrieve and not CS.lead_objspd < 2:
+        elif self.gap_by_spd_on and self.gap_by_spd_on_sw_trg and CS.cruiseGapSet != self.gap_by_spd_gap[1] and ((self.gap_by_spd_spd[0] <= CS.clu_Vanz < self.gap_by_spd_spd[1]+self.gap_by_spd_on_buffer2) or self.gap_by_spd_gap2) and not self.try_early_stop_retrieve and not CS.lead_objspd < 0:
           self.gap_by_spd_gap1 = False
           self.gap_by_spd_gap2 = True
           self.gap_by_spd_gap3 = False
@@ -1492,7 +1493,7 @@ class CarController(CarControllerBase):
             self.refresh_time = interp(CS.cruiseGapSet, [1,2,4], [2,1,3])
           elif self.gap_by_spd_gap[1] == 4:
             self.refresh_time = interp(CS.cruiseGapSet, [1,2,3], [3,2,1])
-        elif self.gap_by_spd_on and self.gap_by_spd_on_sw_trg and CS.cruiseGapSet != self.gap_by_spd_gap[2] and ((self.gap_by_spd_spd[1] <= CS.clu_Vanz < self.gap_by_spd_spd[2]+self.gap_by_spd_on_buffer3) or self.gap_by_spd_gap3) and not self.try_early_stop_retrieve and not CS.lead_objspd < 1:
+        elif self.gap_by_spd_on and self.gap_by_spd_on_sw_trg and CS.cruiseGapSet != self.gap_by_spd_gap[2] and ((self.gap_by_spd_spd[1] <= CS.clu_Vanz < self.gap_by_spd_spd[2]+self.gap_by_spd_on_buffer3) or self.gap_by_spd_gap3) and not self.try_early_stop_retrieve and not CS.lead_objspd < 0:
           self.gap_by_spd_gap1 = False
           self.gap_by_spd_gap2 = False
           self.gap_by_spd_gap3 = True
--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -565,7 +565,7 @@ class CarState(CarStateBase):
 
     if not self.exp_long:
       ret.cruiseAccStatus = cp_scc.vl["SCC12"]["ACCMode"] == 1
-      ret.driverAcc = self.driverOverride
+      ret.driverAcc = bool(self.driverOverride)
       ret.aReqValue = cp_scc.vl["SCC12"]["aReqValue"]
       self.highway_cam = cp_scc.vl["SCC11"]["Navi_SCC_Camera_Act"]
       self.lead_distance = cp_scc.vl["SCC11"]["ACC_ObjDist"]
@@ -723,12 +723,12 @@ class CarState(CarStateBase):
       lead_objspd = cp_cruise_info.vl["SCC_CONTROL"]["ACC_ObjRelSpd"]
       self.lead_objspd = lead_objspd * CV.MS_TO_KPH
       self.scc_control = copy.copy(cp_cruise_info.vl["SCC_CONTROL"])
-      self.driverOverride = cp_cruise_info.vl["SCC_CONTROL"]["ACCMode"] == 2
+      self.driverOverride = cp.vl["TCS"]["DriverOverride"]
       if self.driverOverride:
         self.driverAcc_time = 100
       elif self.driverAcc_time:
         self.driverAcc_time -= 1
-      ret.driverAcc = self.driverOverride
+      ret.driverAcc = bool(self.driverOverride)
 
     # Manual Speed Limit Assist is a feature that replaces non-adaptive cruise control on EV CAN FD platforms.
     # It limits the vehicle speed, overridable by pressing the accelerator past a certain point.
