8badbf51971290883e562c9c873fb7c90ec7664e
jneibling <74702297+jneibling@users.noreply.github.com>
2025-09-13 17:28:54 -0600

    Update interface.py

--- opendbc_repo/opendbc/car/hyundai/interface.py
+++ opendbc_repo/opendbc/car/hyundai/interface.py
@@ -190,7 +190,7 @@ class CarInterface(CarInterfaceBase):
     ret.steerActuatorDelay = params.get("SteerActuatorDelayAdj") * 0.01   #0.1
     ret.steerLimitTimer = params.get("SteerLimitTimerAdj") * 0.01   #0.4
 
-    ret.smoothSteer.method = params.get("KisaSteerMethod")   # 1
+    ret.smoothSteer.method = int ( params.get("KisaSteerMethod") )
     ret.smoothSteer.maxSteeringAngle = params.get("KisaMaxSteeringAngle")   # 90
     ret.smoothSteer.maxDriverAngleWait = params.get("KisaMaxDriverAngleWait")  # 0.002
     ret.smoothSteer.maxSteerAngleWait = params.get("KisaMaxSteerAngleWait")   # 0.001  # 10 sec
